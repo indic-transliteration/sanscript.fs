664005340b62abefde48def56076c6e05457efd4
arvindd <1006084+arvindd@users.noreply.github.com>
2021-07-30 09:08:18 +0530

    Added test and example projects.

---
+++ examples/csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
