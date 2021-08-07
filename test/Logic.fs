497bf7f490992b0a722a8494cef21586cfdc5361
arvindd <1006084+arvindd@users.noreply.github.com>
2021-08-07 10:46:38 +0530

    Tests updated with Tomlet.

--- test/Logic.fs
+++ test/Logic.fs
@@ -1,14 +1,9 @@
 namespace FSharp.Indic.Sanscript.Tests
 
-open System.IO
 open FSharp.Indic.Sanscript
 
 module Logic =
-  let stream s =
-    new StringReader(s)
-    
   let isGoodScheme s = 
-    let s = stream s
     let res = Sanscript.decodeScheme s
     match res with
     | Ok _ -> true
